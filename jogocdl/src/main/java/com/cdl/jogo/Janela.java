42d5b6db2923aad89191ad62e958f4aff73d1ce6
davidtahimalvesbrito <davidtahim@gmail.com>
2026-02-23 19:43:59 -0300

    adiciona a classe Janela com painel para desenho na interface gráfica

--- jogocdl/src/main/java/com/cdl/jogo/Janela.java
+++ jogocdl/src/main/java/com/cdl/jogo/Janela.java
@@ -1,8 +1,24 @@
 package com.cdl.jogo;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Graphics;
 
 public class Janela extends JFrame {
+    private JPanel tela;
+
     public Janela(){
+
+        tela = new JPanel(){
+
+            public void paintComponent(Graphics g){
+                // Aqui você pode desenhar o que quiser usando o objeto Graphics 'g'
+                
+            }
+
+        };
+
+        super.getContentPane().add(tela);
+
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(640,800);
         setVisible(true);
