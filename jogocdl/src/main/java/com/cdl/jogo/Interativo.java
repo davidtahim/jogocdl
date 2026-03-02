51958ea716f20c45fb2044e08f208a306057728f
davidtahimalvesbrito <davidtahim@gmail.com>
2026-03-02 19:13:17 -0300

    adiciona a classe Interativo com estrutura básica para a interface gráfica

---
+++ jogocdl/src/main/java/com/cdl/jogo/Interativo.java
@@ -0,0 +1,22 @@
+package com.cdl.jogo;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+
+public class Interativo extends JFrame {
+    private JPanel tela;
+    private int px;
+    private int py;
+    private boolean jogando = true;
+    private final int FPS = 1000/20;
+
+
+    
+
+
+
+
+}
